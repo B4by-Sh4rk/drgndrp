e3a2b5533a8b7b6b8ada748e3533e4ab0813d970
Svetlana Smirnova <111796441+B4by-Sh4rk@users.noreply.github.com>
2024-03-21 13:42:42 +0300

    1

---
+++ index.js
@@ -0,0 +1,205 @@
+const test_1 = document.getElementById('test_1');
+const test_2 = document.getElementById('test_2');
+const test_3 = document.getElementById('test_3');
+const test_1_div = document.getElementById('test_1_div');
+const test_2_div = document.getElementById('test_2_div');
+const test_3_div = document.getElementById('test_3_div');
+
+test_1.style.color = 'red';
+test_1_div.style.display = 'block';
+
+test_1.onclick = function () {
+    li_click(test_1, test_1_div);
+};
+test_2.onclick = function () {
+    li_click(test_2, test_2_div);
+};
+test_3.onclick = function () {
+    li_click(test_3, test_3_div);
+};
+
+function li_click(test_q, test_q_div) {
+    document.querySelectorAll('li').forEach(element => {
+        element.style.color = 'black';
+    });
+    document.querySelectorAll('div.main').forEach(element => {
+        element.style.display = 'none';
+    });
+    test_q.style.color = 'red';
+    test_q_div.style.display = 'block';
+}
+
+
+
+
+
+
+/* test_1 */
+let thumb = slider.querySelector('.thumb');
+
+thumb.onmousedown = function (event) {
+    event.preventDefault();
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    function onMouseUp() {
+        document.removeEventListener('mouseup', onMouseUp);
+        document.removeEventListener('mousemove', onMouseMove);
+    }
+
+    let shiftX = event.clientX - thumb.getBoundingClientRect().left;
+
+    function onMouseMove(event) {
+        let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+        if (newLeft < 0) {
+            newLeft = 0;
+        }
+        let rightEdge = slider.offsetWidth - thumb.offsetWidth;
+        if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+        }
+        thumb.style.transform = 'translateX(' + newLeft + 'px)';
+
+        if (thumb.style.transform == 'translateX(300px)') {
+            console.log('100%');
+        }
+
+    }
+};
+
+
+
+
+/* test_2 */
+document.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+    const draggable = e.target;
+    if (!draggable.classList.contains('draggable')) {
+        return
+    };
+
+    let shiftX = e.clientX - draggable.getBoundingClientRect().left;
+    let shiftY = e.clientY - draggable.getBoundingClientRect().top;
+
+    draggable.style.position = 'absolute';
+    draggable.style.transform = 'translate(' + e.pageX - shiftX + 'px,' + e.pageY - shiftY + 'px)';
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    function onMouseMove(e) {
+        const minX = window.pageXOffset;
+        const minY = window.pageYOffset;
+        const maxX = document.documentElement.clientWidth + window.pageXOffset - draggable.offsetWidth;
+        const maxY = document.documentElement.clientHeight + window.pageYOffset - draggable.offsetHeight;
+
+        let x = e.pageX - shiftX;
+        let y = e.pageY - shiftY;
+
+        if (x < minX) {
+            x = minX;
+            window.scrollBy(-10, 0);
+        }
+        if (y < minY) {
+            y = minY;
+            window.scrollBy(0, -10);
+        }
+        if (x > maxX) {
+            x = maxX;
+            window.scrollBy(10, 0);
+        }
+        if (y > maxY) {
+            y = maxY;
+            window.scrollBy(0, 10);
+        }
+
+        draggable.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+    }
+
+    function onMouseUp(e) {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+});
+
+
+/* test 3 */
+let currentDroppable = null;
+let currentDroppable2 = null;
+
+ech.onmousedown = function(event) {
+
+  let shiftX = event.clientX - ech.getBoundingClientRect().left;
+  let shiftY = event.clientY - ech.getBoundingClientRect().top;
+
+  ech.style.position = 'absolute';
+  ech.style.zIndex = 1000;
+  document.body.append(ech);
+
+  moveAt(event.pageX, event.pageY);
+
+  function moveAt(pageX, pageY) {
+    ech.style.left = pageX - shiftX + 'px';
+    ech.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+
+    ech.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    ech.hidden = false;
+
+    if (!elemBelow) return;
+
+    let droppableBelow = elemBelow.closest('.krb3_zone');
+    if (currentDroppable != droppableBelow) {
+      if (currentDroppable) {
+        leaveDroppable(currentDroppable);
+      }
+      currentDroppable = droppableBelow;
+      if (currentDroppable) {
+        enterDroppable(currentDroppable);
+      }
+    }
+
+    let droppableBelow2 = elemBelow.closest('.krb2_zone');
+    if (currentDroppable2 != droppableBelow2) {
+      if (currentDroppable2) {
+        leaveDroppable(currentDroppable2);
+      }
+      currentDroppable2 = droppableBelow2;
+      if (currentDroppable2) {
+        enterDroppable(currentDroppable2);
+      }
+    }
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  ech.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    ech.onmouseup = null;
+  };
+
+};
+
+function enterDroppable(elem) {
+    document.getElementById('krb1_zone').style.opacity = '0';
+    elem.style.opacity = '1';
+    if(elem.classList.contains("krb3_zone")){
+        document.getElementById('krb1_zone').style.opacity = '0';
+        document.querySelector('.msg').classList.add('show_msg')
+    }
+}
+function leaveDroppable(elem) {
+    document.getElementById('krb1_zone').style.opacity = '1';
+  elem.style.opacity = '0';
+  if(elem.classList.contains("krb3_zone")){
+        document.querySelector('.msg').classList.remove('show_msg')
+    }
+}
+
+ech.ondragstart = function() {
+  return false;
+};
