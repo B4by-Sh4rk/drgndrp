28150a97844d45cf8d08db06e7ad89369bde9ace
Svetlana Smirnova <111796441+B4by-Sh4rk@users.noreply.github.com>
2024-03-25 19:05:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ test_1_div.style.display = 'block';
 
 test_1.onclick = function () {
     li_click(test_1, test_1_div);
-    document.getElementById('ech').style.display = 'none';
+    document.getElementById('ech').style.opacity = '0';
+    document.getElementById('ech').style.pointerEvents = 'none';
 
 };
 test_2.onclick = function () {
     li_click(test_2, test_2_div);
-    document.getElementById('ech').style.display = 'none';
+    document.getElementById('ech').style.opacity = '0';
+    document.getElementById('ech').style.pointerEvents = 'none';
 
 };
 test_3.onclick = function () {
     li_click(test_3, test_3_div);
-    document.getElementById('ech').style.display = 'flex';
+    document.getElementById('ech').style.opacity = '1';
+    document.getElementById('ech').style.pointerEvents = 'all';
 };
 
 function li_click(test_q, test_q_div) {
